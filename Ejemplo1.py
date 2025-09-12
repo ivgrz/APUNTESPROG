802f02b839ee4a65ec3f7f1aea9b3dc86617d4c1
ivnan <igutierrezcontreras@danielcastelao.org>
2025-09-12 09:56:41 +0200

    v1

---
+++ Ejemplo1.py
@@ -0,0 +1,31 @@
+#Declaramos variables segun su tipo y los mostramos por pantalla (contenid y tipo)
+
+entero = 5
+decimal = 6.7
+complejo = 4+5j
+#Si quiero poner comillas simples en el texto que sale por pantalla se escribe asi
+cadena = "Ola a 'todos'"
+#Si quiero poner comillas dobles en el texto que sale por pantalla se escribe asi
+otraCadena = 'Ola a "todos"'
+
+# TYPE es una funcion de python para describir la clase de dato que es cierta variable que elijamos
+
+print (cadena, otraCadena)
+print (type(complejo))
+print (type(cadena))
+print (type(otraCadena))
+
+booleano = True
+print (type(booleano))
+
+#Tipos de numeros
+
+numHexadecimal = 0x15A
+numOctal = 0o3457
+real = 0.1e-3
+
+print(type(numHexadecimal))
+print(type(numOctal))
+print(real)
+print(numOctal)
+
