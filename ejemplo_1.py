952646d0fe42fb4df829ba90d461e5bad6048b8d
ivnan <igutierrezcontreras@danielcastelao.org>
2025-09-17 14:39:07 +0200

    v8

---
+++ ejemplo_1.py
@@ -0,0 +1,12 @@
+temp = 24
+porcNubes = 30
+precipitacion = 2
+
+if temp > 16 and porcNubes < 40 and precipitacion == 0:
+    print ("Agarrar toalla")
+    print("Poner bañador")
+    print("ir a playa")
+else:
+    print("Quedarse en casa")
+    print("Ver Netflix")
+    print("Comer palomitas")
