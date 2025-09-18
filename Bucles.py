d944f03cbf81c58fdd169a2a9883a61adc10aa55
ivnan <igutierrezcontreras@danielcastelao.org>
2025-09-18 14:40:03 +0200

    bucles--

--- Bucles.py
+++ Bucles.py
@@ -1,4 +1,5 @@
 # Bucles basicos de python
+# Funcion con while y else
 def controltemp():
     temp = int(input("Dime la temperatura: "))
 
@@ -10,6 +11,30 @@ def controltemp():
     else:
         print(f"Otra operacion")
 
+controltemp()
 
-if __name__ == "__main__":
-    controltemp()
+# Funcion con while segundo ejemplo
+def contar():
+    cont = 0
+
+    while cont <= 20:
+        print(cont)
+        cont += 3
+    if cont >= 21:
+        print("Hemos llegado al final del bucle")
+
+
+contar()
+
+# Funcion con while true, break y continue
+def bucle_whiletrue():
+    cont = 0
+    while True:
+        cont += 1
+        if cont%3 != 0:
+            continue
+        print(cont)
+        if cont > 20:
+            break
+bucle_whiletrue()
+
