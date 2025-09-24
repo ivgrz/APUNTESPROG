434ea8cd2ddbdba3545fea1308dc31d6b3028016
ivnan <igutierrezcontreras@danielcastelao.org>
2025-09-24 13:24:44 +0200

    bucles--for y colecciones--4

--- Bucle_for.py
+++ Bucle_for.py
@@ -12,6 +12,7 @@ def bucle_for1():
 # i += 1 realiza incremento en 1 la variable i
     print(f"La suma de los primeros 8 numeros es: {suma}")
 bucle_for1()
+
 def bucle_for2():
     t = (1,2,3,4,5,6,7,8,9,10)
     suma = 0
@@ -20,3 +21,11 @@ def bucle_for2():
         suma = suma + elemento
     print(f"La suma de todos los numeros es: {suma}")
 bucle_for2()
+
+def bucle_for3():
+    l = [1,2,3,4,5,6,7,8,9,10]
+    suma = 0
+    for i in range(0,8,3): #range(inicio, fin, salto)
+        print(l[i])
+
+bucle_for3()
