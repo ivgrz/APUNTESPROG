02130acd12e70063e59b512900960a88ad7d93c7
ivnan <igutierrezcontreras@danielcastelao.org>
2025-09-24 13:03:25 +0200

    bucles--for y colecciones--

--- Bucles.py
+++ Bucles.py
@@ -38,3 +38,13 @@ def bucle_whiletrue():
             break
 bucle_whiletrue()
 
+def bucle_for():
+
+    t = (1,2,3,4,5,6,7,8,9,10)
+    i = 0
+    suma = 0
+    while i <= 7:
+        suma = suma + t[i]
+        i += 1
+# i += 1 realiza incremento en 1 la variable i
+    print(f"La suma de los primeros 8 numeros es: {suma}")
