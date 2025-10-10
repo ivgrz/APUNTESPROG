19efb31e77108e80c4710705276b74c1d8b9cb13
ivnan <igutierrezcontreras@danielcastelao.org>
2025-10-10 09:47:22 +0200

    Ejercicio_clase_3_terminado

---
+++ Ejercicios_propuestos/Ejercicio_clase_3.py
@@ -0,0 +1,10 @@
+"""
+Hacer una funcion que elmine los espacios en blanco al inicio y al final de una cadena de texto.
+Pero deje los espacios en blanco que haya entre palabras.
+
+"""
+def eliminar_espacios(cadena):
+
+    return cadena.strip()
+
+print(eliminar_espacios(" una cadena cualquiera "))
