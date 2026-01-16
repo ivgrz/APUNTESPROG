d64750c1cd3784a37747ccf67ae19cda9ac0d7f5
vnangz <igutierrezcontreras@danielcastelao.org>
2026-01-16 11:39:50 +0100

    Clase_Ficheros_1

--- 2do_Trimestre/Ficheros/FicheroConPickle.py
+++ 2do_Trimestre/Ficheros/FicheroConPickle.py
@@ -29,5 +29,19 @@ with open("fecha.dat", "wb") as fichero:
 	pickle.dump(f_hora, fichero)
 # Usamos .dump() para volcar el objeto en el fichero
 with open("fecha.dat", "rb") as fichero:
+# Usamos .load() para cargar el objeto del fichero
 	dic = pickle.load(fichero)
 	print(f"La hora de lectura es {dic['hora']} : {dic['minuto']} : {dic['segundo']} ")
+
+opcionesUsuario = {
+	'theme' : 'dark',
+	'font_size' : 14,
+	'show_line_numbers' : True
+}
+
+with open('opciones.dat','wb') as fich:
+	pickle.dump(opcionesUsuario, fich)
+
+with open('opciones.dat','rb') as fich:
+	ops = pickle.load(fich)
+	print(ops)
