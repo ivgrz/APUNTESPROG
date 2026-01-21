38e63bcc56dd261be736bee0502835ad24d002de
vnangz <igutierrezcontreras@danielcastelao.org>
2026-01-21 14:21:52 +0100

    Clase_Sets

---
+++ 2do_Trimestre/UsoDeSets/Sets.py
@@ -0,0 +1,54 @@
+# Un Set (conjunto) en Python es una colección desordenada de elementos únicos.
+# Sus principales características son:
+# 1. No permite duplicados: si añades un elemento que ya existe, no sucede nada.
+# 2. Es desordenado: los elementos no tienen un índice o posición fija.
+# 3. Elementos inmutables: los elementos del set deben ser de tipos que no cambien (como strings, números o tuplas).
+
+# Crear un set con elementos
+frutas = {"manzana", "banana", "cereza"}
+
+# add(): Añadir un elemento
+frutas.add("naranja")
+
+# update(): Añadir múltiples elementos (pueden ser listas u otros sets)
+frutas.update(["uva", "mango"])
+
+# remove(): Elimina un elemento (lanza error si no existe)
+frutas.remove("banana")
+
+# discard(): Elimina un elemento (NO lanza error si el elemento no existe)
+frutas.discard("pera")
+
+print(frutas)
+
+numeros_repetidos = [1, 2, 2, 3, 4, 4, 4, 5]
+
+# Convertir la lista a set elimina los duplicados automáticamente
+numeros_unicos = list(set(numeros_repetidos))
+
+print(numeros_unicos) # Resultado: [1, 2, 3, 4, 5]
+
+colores = {"rojo", "verde", "azul"}
+
+# pop() - Quita uno al azar
+eliminado = colores.pop()
+
+# clear() - Vacía el set
+colores.clear()
+
+A = {1, 2, 3}
+B = {1, 2, 3, 4, 5}
+C = {10, 20}
+
+print(A.issubset(B))    # True (A está dentro de B)
+print(B.issuperset(A))  # True (B contiene a A)
+print(A.isdisjoint(C))  # True (No comparten nada)
+
+mis_apps = {"WhatsApp", "Instagram", "TikTok"}
+apps_productividad = {"WhatsApp", "Slack", "Notion"}
+
+# Solo me quedo con las que sirven para trabajar
+mis_apps.intersection_update(apps_productividad)
+
+print(mis_apps) # Resultado: {'WhatsApp'}
+
