f3884b0556445134d9645d172bf2ef9dffd90e9a
vnangz <igutierrezcontreras@danielcastelao.org>
2026-02-12 14:06:21 +0100

    SegundoControlCorreccion_PrimerGrupo_completo

--- Correccion_examen/ControlSegundaEvaluacion/PrimerGrupo/main.py
+++ Correccion_examen/ControlSegundaEvaluacion/PrimerGrupo/main.py
@@ -1,11 +1,19 @@
 from Partida import Partida
 from jugador import jugadori
 if __name__ == '__main__':
-	x1 = jugadori("Pepe")
-	x2 = jugadori("Ana")
-	x3 = jugadori("Xoan")
-	x4 = jugadori("Mario")
+	x1 = jugadori("Pepe",30,25)
+	x2 = jugadori("Ana",35,28)
+	x3 = jugadori("Xoan",40,33)
+	x4 = jugadori("Mario",14,22)
 
 	p1 = Partida(4)
 
 	p1.add_jugador(x1)
+	p1.add_jugador(x2)
+	p1.add_jugador(x3)
+	p1.add_jugador(x4)
+
+	p1.get_jugador_con_min_puntos(15)
+
+
+
