4a576b3e4868bcb54f7c099cbe6d944fc659aa04
vnangz <igutierrezcontreras@danielcastelao.org>
2026-02-13 11:36:53 +0100

    Correccion_Grupo1_Update

--- Correccion_examen/ControlSegundaEvaluacion/PrimerGrupo/JuegoImpostor.py
+++ Correccion_examen/ControlSegundaEvaluacion/PrimerGrupo/JuegoImpostor.py
@@ -1,42 +1,61 @@
 from jugador import jugadori
 from Partida import Partida
+
+
 class JuegoImpostor:
-	def __init__(self):
-		self.__partida = None
-		self.iniciarJuego()
-		ganador = self.FaseJuego()
-		self.FaseFinal(ganador)
+    def __init__(self):
+        numJugadores = int(input("Cuantos jugadores van a jugar?: "))
+        self.__partida = Partida(numJugadores)
+        self.iniciarJuego(numJugadores)
+        ganador = self.FaseJuego()
+        self.FaseFinal(ganador)
 
-	def iniciarJuego(self):
-		numJugadores = int(input("Cuantos jugadores van a jugar?: "))
-		self.__partida = Partida(numJugadores)
+    def iniciarJuego(self, numJugadores):
 
-		for _ in range(numJugadores):
-			nombreJugador = input("Escribe el nombre del jugador: ")
-			j = jugadori(nombreJugador)
-			self.__partida.add_jugador(j)
+        for i in range(numJugadores):
+            nombreJugador = input(
+                f"Escribe el nombre del jugador {i+1}/{numJugadores}: ")
+            j = jugadori(nombreJugador)
+            if self.__partida.add_jugador(j) == -1:
+                print("No se pudo añadir el jugador, se alcanzo el maximo.")
+                break
 
-	def FaseJuego(self):
-		nombreJugador = None
-		while (True):
-			nombreJugador = input("Escribe el nombre del jugador: ")
-			jugador = self.__partida.get_jugador(nombreJugador)
-			if jugador is not None:
-				puntos = input(f"Cuantos puntos hizo {jugador}: ")
-				jugador.sumar_puntos(int(puntos))
-				if jugador.puntuacion >= 50:
-					return nombreJugador
+        print(
+            f"\nSe registraron {numJugadores} jugadores. Comienza la fase de juego!\n")
 
+    def FaseJuego(self):
+        nombreJugador = None
+        while (True):
+            nombreJugador = input(
+                "Escribe el nombre del jugador que anoto puntos: ")
+            jugador = self.__partida.get_jugador(nombreJugador)
+            if jugador is None:
+                print(
+                    f"No existe un jugador con el nombre '{nombreJugador}'. Intenta de nuevo.")
+                continue
+            puntos = input(f"Cuantos puntos hizo {jugador}: ")
+            jugador.sumar_puntos(int(puntos))
+            if jugador.puntuacion >= 50:
+                return nombreJugador
 
-	def FaseFinal(self, ganador):
-		for jugador in self.__partida.get_jugadores():
-			print(f"El jugador {jugador.nombre} tiene {jugador.puntuacion} puntos")
-		print(f"El jugador ganador es {ganador} !!!  ")
-		print("Fin del juego")
+    def FaseFinal(self, ganador):
+        for jugador in self.__partida.get_jugadores():
+            print(
+                f"El jugador {jugador.nombre} tiene {jugador.puntuacion} puntos")
+        print(f"El jugador ganador es {ganador} !!!  ")
+        print("Fin del juego")
+
+    def juego_por_consola(self,):
+        self.iniciarJuego(Partida.get_jugadores)
+        nombreGanador = self.FaseJuego()
+        self.FaseFinal(nombreGanador)
+
+    def juego_por_fichero(self, ruta):
+        ruta = open('doc.txt', 'r')
+        for linea in ruta:
+            print(linea)
+        ruta.close()
 
 
 if __name__ == "__main__":
-	juego = JuegoImpostor()
-
-
-
+    juego = JuegoImpostor()
